e5e38b3d81ce33996ac3df41421760ff2f009c70
Lucack <lucak260@gmail.com>
2022-12-16 18:21:12 -0300

    6 Section Completed

---
+++ Section 6 - Input and Output/reading_file.py
@@ -0,0 +1,6 @@
+f = open(r"C:\Users\Lucas\Desktop\GitHub\Learn-Python-The-Complete-Python-Programming-Course\Section 6 - Input and Output\Testfile.txt")
+print(f.read())
+print(f.read()) # dont read twices
+
+print(f.tell()) # return '90' 
+
