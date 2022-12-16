9732181ccdfe4b77655e6e4c30d848ab877359eb
Lucack <lucak260@gmail.com>
2022-12-16 19:29:14 -0300

    Finishing my guess_game

--- Section 7 - Python Modules/guessing_game.py
+++ Section 7 - Python Modules/guessing_game.py
@@ -2,12 +2,10 @@ import random
 number = random.randint(0,20)
 print("                   ---- Welcome to the Guessing Game!! ----   created by Lucas Santana")
 
-
-
-def guess():
+def guess_and_verify(): # function to obtain only int inputs and between 0 and 20
     while True:
         try: 
-            num = int(input("Digite um NÚMERO entre 0 e 20: "))
+            num = int(input("Digite um número entre 0 e 20: "))
             if num>20 or num<0:
                 raise ValueError("Seu número está incorreto!")
 
@@ -18,23 +16,20 @@ def guess():
 
     return num
 
-guessNumber = guess()
+guessNumber = guess_and_verify()
 c=1
 
 while guessNumber != number:
     if guessNumber < number:
         print("Digite um número maior para acertar ")
-        guessNumber = guess()
-        
+        guessNumber = guess_and_verify()
         c+=1
     
     elif guessNumber > number:
         print("Digite um número menor para acertar ")
-        guessNumber = guess()
-        
+        guessNumber = guess_and_verify()
         c+=1
-
-            
+           
 if guessNumber == number:
     print("Parabéns! Você acertou o número sorteado!")
     print("Você preisou de",c,"tentativas no total")
