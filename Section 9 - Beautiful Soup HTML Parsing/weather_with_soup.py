a7150a4ae5de76e5a0b8f40db4ebda52050d30b1
Lucack <lucak260@gmail.com>
2022-12-17 16:00:19 -0300

    include try/except functions

--- Section 9 - Beautiful Soup HTML Parsing/weather_with_soup.py
+++ Section 9 - Beautiful Soup HTML Parsing/weather_with_soup.py
@@ -4,35 +4,38 @@ import requests
 
 # https://g1.globo.com/previsao-do-tempo/sp/sao-paulo.ghtml
 
-# try:
-url = "https://g1.globo.com/previsao-do-tempo/"
-sigla = input("Digite a sigla do estado: ")
-cid = input("Agora, digite o nome da cidade: ")
+try:
+    url = "https://g1.globo.com/previsao-do-tempo/"
+    sigla = input("Digite a sigla do estado: ")
+    cid = input("Agora, digite o nome da cidade: ")
 
-#sigla,cid = "sp","sao-paulo"
-cidPrint = cid
-cid = cid.replace(" ","-")
-
-
-url+= sigla+"/"+cid+".ghtml"
-
-data =  requests.get(url)
-#data= data.text
-soup = BeautifulSoup(data.text,"html.parser")
-
-day = soup.find('p',{'class':'forecast-header__date'}).string
-local = soup.find('p',{'class':'forecast-header__place'}).string
-weather = soup.find('p',{'class':'forecast-header__summary'}).string
-
-
-maxTemp = soup.find('div',{'class':'forecast-today__temperature forecast-today__temperature--max'}).contents
-minTemp = soup.find('div',{'class':'forecast-today__temperature forecast-today__temperature--min'}).contents
-
-print("A previsão do tempo para",day, "em", local,"é: \n  - ", weather)
-print("  -  Com máxima de", maxTemp[0])
-print("  -  E mínima de", minTemp[0])
+    #sigla,cid = "sp","sao-paulo"
+    cidPrint = cid
+    sigla = sigla.replace(" ","")
+    cid = cid.replace(" ","-",1)
+    cid = cid.replace(" ","")
 
 
 
-#except:
-#      print("Desculpe, não temos informações sobre este lugar, confira e tente novamente")
+    url+= sigla+"/"+cid+".ghtml"
+
+    data =  requests.get(url)
+    #data= data.text
+    soup = BeautifulSoup(data.text,"html.parser")
+
+    day = soup.find('p',{'class':'forecast-header__date'}).string
+    local = soup.find('p',{'class':'forecast-header__place'}).string
+    weather = soup.find('p',{'class':'forecast-header__summary'}).string
+
+
+    maxTemp = soup.find('div',{'class':'forecast-today__temperature forecast-today__temperature--max'}).contents
+    minTemp = soup.find('div',{'class':'forecast-today__temperature forecast-today__temperature--min'}).contents
+
+    print("A previsão do tempo para",day, "em", local,"é: \n  - ", weather)
+    print("  -  Com máxima de", maxTemp[0])
+    print("  -  E mínima de", minTemp[0])
+
+
+
+except:
+      print("Desculpe, não temos informações sobre este lugar, confira e tente novamente")
