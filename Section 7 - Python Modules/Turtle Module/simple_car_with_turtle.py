c3ffe53676a9792cc73bc2814fa427941423d0d2
Lucack <lucak260@gmail.com>
2022-12-17 22:51:49 -0300

    7 Section Completed

--- Section 7 - Python Modules/Turtle Module/simple_car_with_turtle.py
+++ Section 7 - Python Modules/Turtle Module/simple_car_with_turtle.py
@@ -5,44 +5,44 @@ t = turtle.Pen()
 t.color(1,0,0)
 time.sleep(0.1)
 t.begin_fill()
-t.forward(180) # -->
+t.forward(2*180) # -->
 t.left(90)
-t.forward(40) # ^
+t.forward(2*40) # ^
 t.left(90)
-t.forward(40) # <
+t.forward(2*40) # <
 t.right(90)
-t.forward(40) # ^
+t.forward(2*40) # ^
 t.left(90)
-t.forward(100) # < top
+t.forward(2*100) # < top
 t.left(90)
-t.forward(40)
+t.forward(2*40)
 t.right(90)
-t.forward(40)
+t.forward(2*40)
 t.left(90)
-t.forward(40)
+t.forward(2*40)
 
 t.end_fill() #car sctructure
 
 t.up()
 t.left(90)
-t.forward(25)
+t.forward(2*25)
 t.right(90)
-t.forward(5)
+t.forward(2*5)
 t.color(0,0,0) # black weeels
 t.down()
 t.begin_fill()
-t.circle(20)
+t.circle(2*20)
 t.end_fill() # first wheel
 
 t.up()
 t.left(90)
-t.forward(100)
+t.forward(2*100)
 t.right(90)
 
 
 t.down()
 t.begin_fill()
-t.circle(20)
+t.circle(2*20)
 t.end_fill() # second wheel
 time.sleep(2)
 
